6b8906a5899e9904da283ee06f6d09e0eca9f140
Duvan Prieto <duvan.prieto.ext@holcim.com>
2025-03-15 22:40:47 -0500

    build: Adds validator classes, adds eslint normal rules around decorators

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,9 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn'
     },
